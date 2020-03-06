5b20805ad4512978d81d9c8419b94d405998379d
ROEL DAMALERIO <damalerioroel@gmail.com>
2020-03-07 04:26:41 +0800

    SGE11 Survey form

--- assets/js/sge-script.js
+++ assets/js/sge-script.js
@@ -27,16 +27,37 @@ function openTabContent(evt, linkName) {
 
   evt.currentTarget.className += " active";
 }
+
+function clearCheckbox(){
+  var x = document.getElementsByClassName("chk");
+  for (i = 0; i < x.length; i++) {
+    x[i].checked = false;
+  }
+}
 //Survey Tab//
 var currentTab = 0; // Current tab is set to be the first tab (0)
 showTab(currentTab); // Display the current tab
 
 function showTab(n){
+    //Check if argument is not pass
+    if(n === undefined){
+      n = currentTab + 1;
+      currentTab++;
+    }else{
+      currentTab = n;
+    }
+      
     // This function will display the specified tab of the form...
+    var y = document.getElementsByClassName("tab");
+    for (i = 0; i < y.length; i++) {
+      y[i].style.display = "none";
+    }
+
     var x = document.getElementsByClassName("tab");
     x[n].style.display = "block";
 
     //... and fix the Previous/Next buttons:
+    /*
     if (n == 0) {
       document.getElementById("prevBtn").style.display = "none";
     } else {
@@ -47,18 +68,21 @@ function showTab(n){
     } else {
       document.getElementById("nextBtn").innerHTML = "Next";
     }
-    
+    */
     //... and run a function that will display the correct step indicator:
     fixStepIndicator(n)
 }
 function fixStepIndicator(n) {
   // This function removes the "active" class of all steps...
   var i, x = document.getElementsByClassName("step");
+  var y = document.getElementsByClassName("nav-cat");
   for (i = 0; i < x.length; i++) {
     x[i].className = x[i].className.replace(" active", "");
+    y[i].className = y[i].className.replace(" active", "");
   }
   //... and adds the "active" class on the current step:
   x[n].className += " active";
+  y[n].className += " active";
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {  
