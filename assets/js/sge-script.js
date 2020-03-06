6e98604d504cbc56a0e36999256f5996cfe46c2b
ROEL G. DAMALERIO <damalerioroel@gmail.com>
2020-03-06 20:21:02 +0800

    SGE 11 initial

--- assets/js/sge-script.js
+++ assets/js/sge-script.js
@@ -26,7 +26,39 @@ function openTabContent(evt, linkName) {
   }
 
   evt.currentTarget.className += " active";
-  
+}
+//Survey Tab//
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+
+function showTab(n){
+    // This function will display the specified tab of the form...
+    var x = document.getElementsByClassName("tab");
+    x[n].style.display = "block";
+
+    //... and fix the Previous/Next buttons:
+    if (n == 0) {
+      document.getElementById("prevBtn").style.display = "none";
+    } else {
+      document.getElementById("prevBtn").style.display = "inline";
+    }
+    if (n == (x.length - 1)) {
+      document.getElementById("nextBtn").innerHTML = "Submit";
+    } else {
+      document.getElementById("nextBtn").innerHTML = "Next";
+    }
+    
+    //... and run a function that will display the correct step indicator:
+    fixStepIndicator(n)
+}
+function fixStepIndicator(n) {
+  // This function removes the "active" class of all steps...
+  var i, x = document.getElementsByClassName("step");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  //... and adds the "active" class on the current step:
+  x[n].className += " active";
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {  
