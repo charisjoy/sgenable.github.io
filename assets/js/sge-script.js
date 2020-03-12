1332d35622997c26eef41d299fb5be413de7f095
ROEL G. DAMALERIO <damalerioroel@gmail.com>
2020-03-12 14:34:53 +0800

    a

--- assets/js/sge-script.js
+++ assets/js/sge-script.js
@@ -213,8 +213,9 @@ var mobButton = 0;
 function showMobileMenu(x){
 
   x.classList.toggle("fa-times");
-  return;
   
+  return;
+
   if(mobButton == 0){
     var carousel = document.getElementById("carouselExampleCaptions12");
     carousel.setAttribute( 'style', 'display:none');
